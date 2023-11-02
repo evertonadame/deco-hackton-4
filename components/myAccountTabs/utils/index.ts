498329d483ade7bbb7da6eeeaa5b0a033ebb3ab6
Everton <evertonadame4@gmail.com>
2023-11-02 11:48:17 -0300

    feat: my account section

---
+++ components/myAccountTabs/utils/index.ts
@@ -0,0 +1,29 @@
+
+export function formatDocumentByType(document: string) {
+    switch (document.length) {
+        case 11:
+            return document.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+        case 14:
+            return document.replace(
+                /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+                "$1.$2.$3/$4-$5",
+            );
+        default:
+            return document;
+    }
+}
+
+export function formatPhone(phone: string) {
+    switch (phone.length) {
+        case 10:
+            return phone.replace(/(\d{2})(\d{4})(\d{4})/, "($1) $2-$3");
+        case 11:
+            return phone.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+        default:
+            return phone;
+    }
+}
+
+export function formatBirthDate(birthDate: string) {
+    return birthDate.replace(/(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
+}
--- fresh.gen.ts
+++ fresh.gen.ts
@@ -16,15 +16,16 @@ import * as $$9 from "./islands/Header/Cart/vtex.tsx";
 import * as $$10 from "./islands/Header/Cart/wake.tsx";
 import * as $$11 from "./islands/Header/Drawers.tsx";
 import * as $$12 from "./islands/Header/Searchbar.tsx";
-import * as $$13 from "./islands/MyAccount.tsx";
-import * as $$14 from "./islands/Newsletter.tsx";
-import * as $$15 from "./islands/OutOfStock.tsx";
-import * as $$16 from "./islands/ProductImageZoom.tsx";
-import * as $$17 from "./islands/SearchControls.tsx";
-import * as $$18 from "./islands/ShippingSimulation.tsx";
-import * as $$19 from "./islands/SliderJS.tsx";
-import * as $$20 from "./islands/SmartTab.tsx";
-import * as $$21 from "./islands/WishlistButton.tsx";
+import * as $$13 from "./islands/MyAccount/MyAccount.tsx";
+import * as $$14 from "./islands/MyAccount/components/MyAccountTab.tsx";
+import * as $$15 from "./islands/Newsletter.tsx";
+import * as $$16 from "./islands/OutOfStock.tsx";
+import * as $$17 from "./islands/ProductImageZoom.tsx";
+import * as $$18 from "./islands/SearchControls.tsx";
+import * as $$19 from "./islands/ShippingSimulation.tsx";
+import * as $$20 from "./islands/SliderJS.tsx";
+import * as $$21 from "./islands/SmartTab.tsx";
+import * as $$22 from "./islands/WishlistButton.tsx";
 
 const manifest = {
   routes: {
@@ -44,15 +45,16 @@ const manifest = {
     "./islands/Header/Cart/wake.tsx": $$10,
     "./islands/Header/Drawers.tsx": $$11,
     "./islands/Header/Searchbar.tsx": $$12,
-    "./islands/MyAccount.tsx": $$13,
-    "./islands/Newsletter.tsx": $$14,
-    "./islands/OutOfStock.tsx": $$15,
-    "./islands/ProductImageZoom.tsx": $$16,
-    "./islands/SearchControls.tsx": $$17,
-    "./islands/ShippingSimulation.tsx": $$18,
-    "./islands/SliderJS.tsx": $$19,
-    "./islands/SmartTab.tsx": $$20,
-    "./islands/WishlistButton.tsx": $$21,
+    "./islands/MyAccount/MyAccount.tsx": $$13,
+    "./islands/MyAccount/components/MyAccountTab.tsx": $$14,
+    "./islands/Newsletter.tsx": $$15,
+    "./islands/OutOfStock.tsx": $$16,
+    "./islands/ProductImageZoom.tsx": $$17,
+    "./islands/SearchControls.tsx": $$18,
+    "./islands/ShippingSimulation.tsx": $$19,
+    "./islands/SliderJS.tsx": $$20,
+    "./islands/SmartTab.tsx": $$21,
+    "./islands/WishlistButton.tsx": $$22,
   },
   baseUrl: import.meta.url,
 };
