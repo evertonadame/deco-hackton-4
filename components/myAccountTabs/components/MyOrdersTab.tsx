468b39ba7667aaeea6b573dee29f0f2dfa6fc9dc
Everton <evertonadame4@gmail.com>
2023-11-03 16:58:44 -0300

    feat: components history

--- components/myAccountTabs/components/MyOrdersTab.tsx
+++ components/myAccountTabs/components/MyOrdersTab.tsx
@@ -1,4 +1,4 @@
-import type { User, Order } from "$store/sections/Account/MyAccount.tsx";
+import type { Order, User } from "$store/sections/Account/MyAccount.tsx";
 import {
   dateFormatter,
   priceFormatter,
@@ -39,7 +39,7 @@ function MyOrdersTab({ orders = [] }: Props) {
     setModalOrderDetails({ isOpen: true, order });
   };
 
-  if (!orders.length)
+  if (!orders.length) {
     return (
       <div className="flex justify-center w-full">
         <h3 className="text-base text-center text-slate-600">
@@ -47,71 +47,76 @@ function MyOrdersTab({ orders = [] }: Props) {
         </h3>
       </div>
     );
+  }
 
-  return modalOrderDetails.isOpen ? (
-    <ModalOrderDetails
-      isOpen={modalOrderDetails.isOpen}
-      closeModal={closeModalOrderDetails}
-      order={modalOrderDetails.order}
-    />
-  ) : (
-    <div className="relative overflow-x-auto">
-      <table className="w-full text-base text-left text-slate-600 border border-slate-200">
-        <thead className="text-base-600 text-xs font-semibold bg-slate-50">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Nº DO PEDIDO
-            </th>
-            <th scope="col" className="px-6 py-3">
-              DATA
-            </th>
-            <th scope="col" className="px-6 py-3">
-              VALOR
-            </th>
-            <th scope="col" className="px-6 py-3">
-              STATUS
-            </th>
-            <th scope="col" className="px-6 py-3">
-              DETALHES
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order, index) => {
-            const { id, date, price, status } = order;
+  return modalOrderDetails.isOpen
+    ? (
+      <ModalOrderDetails
+        isOpen={modalOrderDetails.isOpen}
+        closeModal={closeModalOrderDetails}
+        order={modalOrderDetails.order}
+      />
+    )
+    : (
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-base text-left text-slate-600 border border-slate-200">
+          <thead className="text-base-600 text-xs font-semibold bg-slate-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Nº DO PEDIDO
+              </th>
+              <th scope="col" className="px-6 py-3">
+                DATA
+              </th>
+              <th scope="col" className="px-6 py-3">
+                VALOR
+              </th>
+              <th scope="col" className="px-6 py-3">
+                STATUS
+              </th>
+              <th scope="col" className="px-6 py-3">
+                DETALHES
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => {
+              const { id, date, price, status } = order;
 
-            return (
-              <tr
-                className={
-                  index !== orders.length - 1 ? "border-b border-slate-200" : ""
-                }
-              >
-                <th className="px-6 py-4 font-normal">{id}</th>
-                <td className="px-6 py-4">
-                  {dateFormatter.format(new Date(date))}
-                </td>
-                <td className="px-6 py-4">{priceFormatter.format(price)}</td>
-                <td className="px-6 py-4 flex items-center gap-2">
-                  <div
-                    className={`h-2 w-2 rounded-full ${getStatusColor(status)}`}
-                  />
-                  {getStatusLabel(status)}
-                </td>
-                <td className="px-6 py-4">
-                  <button
-                    className="text-accent hover:underline"
-                    onClick={() => handleViewOrderDetails(order)}
-                  >
-                    Visualizar
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+              return (
+                <tr
+                  className={index !== orders.length - 1
+                    ? "border-b border-slate-200"
+                    : ""}
+                >
+                  <th className="px-6 py-4 font-normal">{id}</th>
+                  <td className="px-6 py-4">
+                    {dateFormatter.format(new Date(date))}
+                  </td>
+                  <td className="px-6 py-4">{priceFormatter.format(price)}</td>
+                  <td className="px-6 py-4 flex items-center gap-2">
+                    <div
+                      className={`h-2 w-2 rounded-full ${
+                        getStatusColor(status)
+                      }`}
+                    />
+                    {getStatusLabel(status)}
+                  </td>
+                  <td className="px-6 py-4">
+                    <button
+                      className="text-accent hover:underline"
+                      onClick={() => handleViewOrderDetails(order)}
+                    >
+                      Visualizar
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
 }
 
 export default MyOrdersTab;
